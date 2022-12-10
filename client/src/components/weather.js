3bc6ca1b4dabbeb210baef88460d457521255afa
sophia pung <sophiagracepung@gmail.com>
2022-12-10 14:45:22 -0800

    changing port

--- client/src/components/weather.js
+++ client/src/components/weather.js
@@ -4,11 +4,12 @@ function LoadWeather() {
     const [humidity, setHumidity] = useState("")
     // A function to fetch the list of students that will be load anytime that list change
     const fetchWeather = () => {
-        fetch('http://localhost:3002/weather')
+        fetch('http://localhost:8080/weather')
         .then((response) => response.json())
         .then((weather) => {
           setHumidity(weather);
         });
+    console.log("humidity", humidity);
     }
     return  (
         <button onClick={fetchWeather}>test</button>
