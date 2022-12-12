64d9d4bff87ff7cd429459530b399682c16ae588
sophia pung <sophiagracepung@gmail.com>
2022-12-11 20:21:03 -0800

    updating API call

--- client/src/components/weather.js
+++ client/src/components/weather.js
@@ -1,38 +1,30 @@
 import { useState } from 'react';
 
 function LoadWeather() {
-    const [zip, setZip] = useState("")
     const [averageTemp, setAverageTemp] = useState("")
-    // A function to fetch the average "feels like" temperature from Berkeley over the next four days.
-    const fetchZip = () => {
-      fetch('http://localhost:8080/zip')
-      .then((response) => response.json())
-      .then((zip) => {
-        setZip(zip);
-      })}; 
-
     const fetchWeather = () => {
-        fetch(`http://localhost:8080/weather?zip=${zip}`)
+        fetch(`http://localhost:8080/weather`)
         .then((response) => response.json())
         .then((weather) => {
           setAverageTemp(weather);
         });
-    const KelvinToFarenheight = (tempInKelvin) => {
-      let farenheight = 1.8*(tempInKelvin-273) + 32;
-      return farenheight
     }
-    console.log(averageTemp)
-    let tempInFarenheight = averageTemp.list.map((weather) => {
-      console.log(weather.main.feels_like)
-      return KelvinToFarenheight(weather.main.feels_like)
-    });
-    console.log(tempInFarenheight)
+    const tempInFarenheight = (averageTemp) => {averageTemp.list.map((weather) => {
+      console.log("test")
+      return 1.8*(weather.main.feels_like-273) + 32;
+    })};
+    function checkBelowForty(temp) {
+      return temp < 40; 
+    };
+    const calculatePercentage = (daysUnderForty) => {
+      return daysUnderForty/96;
     }
-
-    console.log(fetchZip())
 
     return  (
+      <div>
         <button onClick={fetchWeather}>test</button>
+        <p>It will feel like it's below 40 degrees {}% of the time over the next four days. </p>
+      </div>
     )
   };
 
--- server/index.js
+++ server/index.js
@@ -10,25 +10,9 @@ app.use(cors());
 
 app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
 
-app.get("/zip", (req, res) => {
-  let API_KEY = process.env.API_KEY;
-    const url = `http://api.openweathermap.org/geo/1.0/zip?zip=94704,US&appid=${API_KEY}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("ZIP DATA", zipdata)
-        res.send({ ...zipdata });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-})
-
 app.get("/weather", (req, res) => {
-    const zip = req.query.zip;
-    console.log("zip", zip);
     let API_KEY = process.env.API_KEY;
-    const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=122.27&lon=37.87&appid=${API_KEY}`;
+    const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=37.87&lon=-122.26&appid=${API_KEY}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
